4c9f4212c03e6b405fe39b5366fab34dbac3003f
Alejandro <alejandropg140600@hotmail.com>
2021-02-11 14:20:58 +0100

    second

--- src/main/kotlin/com/example/server/GatetesController.kt
+++ src/main/kotlin/com/example/server/GatetesController.kt
@@ -5,22 +5,22 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class GatetesController(private val gatetesRepository : GatetesRepository) {
 
-    @GetMapping("/students")
+    @GetMapping("/gatos")
     fun getAllGatetes() : List<Gatetes> {
         return gatetesRepository.findAll()
     }
 
-    @PostMapping("/students")
+    @PostMapping("/gatos")
     fun insertGatetes(@RequestBody student : Gatetes){
         gatetesRepository.save(student)
     }
 
-    @GetMapping("/student/{id}")
+    @GetMapping("/gatos/{id}")
     fun getGatetes(@PathVariable id : Long) : Gatetes {
         return gatetesRepository.findById(id).get()
     }
 
-    @DeleteMapping("/student/{id}")
+    @DeleteMapping("/gatos/{id}")
     fun deleteGatetes(@PathVariable id : Long){
         gatetesRepository.deleteById(id)
     }
--- src/main/kotlin/com/example/server/LoadDatabase.kt
+++ src/main/kotlin/com/example/server/LoadDatabase.kt
@@ -12,10 +12,13 @@ class LoadDatabase {
     }
 
     @Bean
-    fun initDatabase(studentRepository : GatetesRepository) : CommandLineRunner {
+    fun initDatabase(gatetesRepository : GatetesRepository) : CommandLineRunner {
         return CommandLineRunner { args: Array<String?>? ->
-            logger.info("Preloading " + studentRepository.save(Gatetes(1, "Cats make about 100 different sounds. Dogs make only about 10.")))
-            logger.info("Preloading " + studentRepository.save(Gatetes(2, "Domestic cats spend about 70 percent of the day sleeping and 15 percent of the day grooming.")))
+            logger.info("Preloading " + gatetesRepository.save(Gatetes(1, "Cats make about 100 different sounds. Dogs make only about 10.")))
+            logger.info("Preloading " + gatetesRepository.save(Gatetes(2, "Domestic cats spend about 70 percent of the day sleeping and 15 percent of the day grooming.")))
+            logger.info("Preloading " + gatetesRepository.save(Gatetes(3, "I don't know anything about cats.")))
+            logger.info("Preloading " + gatetesRepository.save(Gatetes(4, "The technical term for a cat’s hairball is a bezoar.")))
+            logger.info("Preloading " + gatetesRepository.save(Gatetes(5, "Cats are the most popular pet in the United States: There are 88 million pet cats and 74 million dogs.")))
         }
 
     }
